88994551b8812d25961ca43517e913e0e9bc19fd
Galaletsang Modimola <mitumeleng28@gmail.com>
2024-10-09 20:03:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Quiz_Game
+{
+     internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            Dictionary<string, string> questions = GetQandA();
+
+            do
+            {
+                Console.WriteLine(questions
+            }
+            while (!exit);
+        }
+
+        public static Dictionary<string, string> GetQandA()
+        {
+            Dictionary<string, string> QandA = new Dictionary<string, string>();
+
+            QandA.Add("C# is a statically typed language.", "True");
+            QandA.Add("C# supports multiple inheritance.", "False");
+            QandA.Add("C# is platform-independent.", "True");
+            QandA.Add("C# is case-sensitive.", "True");
+
+            return QandA;
+        }
+    }
+}
